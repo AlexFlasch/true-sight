e130913cc5d15a199b2fdff77df2232a5cd11fd1
Flascher <alexanderflasch@gmail.com>
2015-04-14 09:15:53 -0500

    Working on states so I can go to different pages.

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,46 @@
-var app = angular.module('trueSight', ['ionic', 'ionMdInput']);
+var ts = angular.module('ts', ['ionic', 'ionMdInput']);
 
-app.run(function($ionicPlatform) {
+ts.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+
+    $stateProvider.state('login', {
+        url: 'login',
+        views: {
+            'login': {
+                templateurl: 'templates/login.html',
+                controller: 'LoginCtrl'
+            }
+        }
+    });
+
+    $stateProvider.state('search', {
+        url: 'search',
+        views: {
+            'search': {
+                templateUrl: 'templates/search.html',
+                controller: 'SearchCtrl'
+            }
+        }
+    });
+
+    $stateProvider.state('results', {
+        url: '/results',
+        views: {
+            'results': {
+                tempalteUrl: 'templates/results.html',
+                controller: 'ResultsCtrl'
+            }
+        }
+    });
+
+    $urlRouterProvider.otherwise('/login');
+});
+
+ts.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -14,30 +54,86 @@ app.run(function($ionicPlatform) {
   });
 });
 
-app.config(function($stateProvider, $urlRouterProvider) {
+ts.controller('LoginCtrl', function($scope, $state) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    $scope.user = {};
 
-  // setup an abstract state for the tabs directive
-    .state('login', {
-    url: "/login",
-    abstract: true,
-    templateUrl: "templates/login.html"
-  })
+    $scope.login = function() {
+        //eventually send data and stuff.
+        // $scope.user = angular.copy(user);
 
-  // Each tab has its own nav history stack:
-
-  .state('search', {
-    url: '/search',
-    views: {
-      'search': {
-        templateUrl: 'templates/search.html',
-        controller: 'DashCtrl'
-      }
+        $state.go('login.search');
+        console.log($state.is('search'));
     }
-  })
-};
+
+});
+
+ts.controller('SearchCtrl', function($scope, $state) {
+
+    //dummy data
+    $scope.results = [{
+        steamId: 0,
+        profilePicture: "urlToPicture",
+        displayName: "Flascher",
+        lastMatch: new Date(),
+    }];
+
+});
+
+ts.factory('Searches', function() {
+
+    //Some more fake testing data
+    var results = [{
+        steamId: 0,
+        displayName: "Flascher",
+        lastMatch: new Date(),
+    }];
+
+    return {
+        all: function() {
+            return results;
+        },
+        get: function(id) {
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].steamId === parseInt(id)) {
+                    return results[i];
+                }
+            }
+            return null;
+        }
+    };
+});
+
+ts.factory('Matches', function() {
+
+  // Some fake testing data
+  var matches = [{
+    id: 0,
+    team: "Dire",
+    date: new Date("1/1/2015"),
+    gameType: "All Pick",
+    duration: "56m",
+    kills: 8,
+    deaths: 2,
+    assists: 4,
+    victory: true,
+    heroId: "Mirana",
+    gpm: 500,
+    xpm: 700,
+    gold: 1800
+  }];
+
+  return {
+    all: function() {
+      return matches;
+    },
+    get: function(matchId) {
+      for (var i = 0; i < matches.length; i++) {
+        if (matches[i].id === parseInt(matchId)) {
+          return matches[i];
+        }
+      }
+      return null;
+    }
+  };
+});
